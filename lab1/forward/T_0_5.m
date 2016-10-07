47e2fb62be4e26a8d69243e1238ee3f92b3faae6
tsangkai <tsangkai.chang@gmail.com>
2016-10-07 16:36:10 -0700

    Add all transition matrices

---
+++ lab1/forward/T_0_5.m
@@ -0,0 +1,6 @@
+function y = T_0_5(t1, t2, t3, t4)
+   y = T_0_1(t1) * T_1_2(t2) * T_2_3(t3) * T_3_4(t4) * T_4_5();
+end
+
+% data source:
+% http://www.theergonomicscenter.com/graphics/Workstation%20Design/Tables.pdf
